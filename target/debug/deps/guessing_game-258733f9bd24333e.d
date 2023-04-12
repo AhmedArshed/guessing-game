69513744a4c3360e240aea0ecfa2378121a40992
Pensive <31464251+AhmedArshed@users.noreply.github.com>
2023-04-12 10:06:30 +0500

    adding guessing game

---
+++ target/debug/deps/guessing_game-258733f9bd24333e.d
@@ -0,0 +1,5 @@
+/home/ahmed/Desktop/guessing_game/target/debug/deps/guessing_game-258733f9bd24333e: src/main.rs
+
+/home/ahmed/Desktop/guessing_game/target/debug/deps/guessing_game-258733f9bd24333e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/ahmed/Desktop/guessing_game/target/debug/guessing_game: /home/ahmed/Desktop/guessing_game/src/main.rs
